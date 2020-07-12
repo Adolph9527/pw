ba6f441a5c1c212e74914fd3e184c63160d1957e
Adolph Wei <log9527@gmail.com>
2020-07-12 15:04:06 +0800

    NOT using while True

--- pw.py
+++ pw.py
@@ -1,14 +1,13 @@
 password = 'a123456'
 times = 1
 try_ = 3
-while True:
+while try_ > 0:
 	key_in = input('請輸入密碼:')
-	if key_in == 'a123456':
+	if key_in == password:
 		print('登入成功')
 		break
 	else:
 		try_ = try_ - 1
 		print('密碼錯誤! 你還有',try_ ,'次機會')
 		times = times + 1
-		if times == 3:
-			break
+		
