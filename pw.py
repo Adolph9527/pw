06a71468f05d5f6d07735c21a79db85a28af1e30
Adolph Wei <log9527@gmail.com>
2020-07-12 15:10:32 +0800

    more briefly

--- pw.py
+++ pw.py
@@ -1,13 +1,15 @@
 password = 'a123456'
-times = 1
+#times = 1
 try_ = 3
 while try_ > 0:
+	try_ = try_ - 1
 	key_in = input('請輸入密碼:')
 	if key_in == password:
 		print('登入成功')
 		break
 	else:
-		try_ = try_ - 1
-		print('密碼錯誤! 你還有',try_ ,'次機會')
-		times = times + 1
 		
+		print('密碼錯誤!')
+		#times = times + 1
+		if try_ > 0:
+			print(' 你還有',try_ ,'次機會')
